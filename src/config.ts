5f19ee1cba6e180a6b1f8303322c299fe2e59c43
archit38 <architbhardwaj88@gmail.com>
2024-11-19 17:17:09 +0000

    fix

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const GOOGLE_MAPS_API_KEY = '';
+export const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY || '';
 
 export const SEARCH_RADIUS = 3000; // 3km radius
 export const RESULTS_COUNT = 20; // Number of results to fetch
